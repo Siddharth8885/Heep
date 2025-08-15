967b5a5aeb6fc7a16d5219a775fde985ae494ed0
Siddharth8885 <siddharthmishra8885@gmail.com>
2025-08-15 23:48:38 +0530

    Delete storage.ts

--- storage.ts
+++
@@ -1,4 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-export async function save(key: string, value: any) { await AsyncStorage.setItem(key, JSON.stringify(value)); }
-export async function load(key: string) { const v = await AsyncStorage.getItem(key); return v ? JSON.parse(v) : null; }
-export async function clearAll() { await AsyncStorage.clear(); }
